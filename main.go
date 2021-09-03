ce16a40d932f6988f23aa3d30a2ee0e18f0c60c9
alexey.svetinskiy <alexey808sv@gmail.com>
2021-09-04 02:11:33 +0400

    init

---
+++ main.go
@@ -0,0 +1,222 @@
+package main // определение файла
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+) // импорт формат библиотеки
+// импорт формат библиотеки
+
+var globalValue1 bool = 1 == 1
+var globalValue2 string = "Hello World"
+
+const globalValue3 string = "HELLO_WORLD"
+const (
+	a = "abc"
+	b = 123
+)
+
+type Circle struct {
+	x float64
+	y float64
+	r float64
+	// x, y, r float64
+}
+
+func main() {
+	// testVariables()
+	// testFtmScanf()
+	// testFor()
+	// testSwitch()
+	// testArray()
+	// testMap()
+	// testCondition()
+	// testFuncPanic()
+	// testPointers()
+	// testStruct()
+	// testStreams()
+	// testPackages()
+}
+
+// ---
+func testVariables() {
+	localValue := "test"
+	fmt.Println(localValue)
+	fmt.Println(globalValue1, globalValue2, globalValue3)
+	fmt.Println(a, b)
+}
+
+// ---
+func testFtmScanf() {
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input) // заполнить переменную input числом ввода
+	output := input * 2
+	fmt.Println(output)
+}
+
+// ---
+func testFor() {
+	for i := 1; i <= 3; i++ {
+		fmt.Println(i)
+	}
+
+	for i := 1; i <= 6; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
+	}
+}
+
+// ---
+func testSwitch() {
+	i := 1
+
+	switch i {
+	case 0:
+		fmt.Println("Zerro")
+	case 1:
+		fmt.Println("One")
+	default:
+		fmt.Println("Unknown number")
+	}
+}
+
+// ---
+func testArray() {
+	x := [5]float64{98, 93, 77, 82, 83} // короткая запись создания массива, тип можн не указ.
+
+	var total float64 = 0
+	for _, value := range x {
+		total += value
+	}
+	fmt.Println(total / float64(len(x)))
+
+	// ---
+
+	var arr [5]int
+	arr[4] = 100
+	arrLength := len(arr) // длина массива
+
+	var float float64 = 0
+	float = float64(len(arr)) // конвертация int в float64
+
+	fmt.Println(arr, arrLength, float)
+
+	// --- Срезы
+	a := make([]float64, 5)     // тип float64 длина 5
+	b := make([]float64, 5, 10) // 10 длина массива, на который указывает срез
+	c := [5]float64{1, 2, 3, 4, 5}
+	cc := c[1:3] // arr[:] эквивалентно arr[0:len(arr)]
+	fmt.Println(a, b, cc)
+}
+
+// ---
+func testMap() {
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+	delete(x, "key")
+	fmt.Println(x["key"])
+	value, status := x["key"]
+	fmt.Println(value, status)
+}
+
+// ---
+func testCondition() {
+	myMap := make(map[string]string)     // создаём мапу
+	myMap["myKey"] = "MYKEY"             // записываем ключ:значение
+	if value, ok := myMap["myKey"]; ok { // если есть value и значение сущесвтует с таким ключом
+		fmt.Println(value, ok)
+	}
+}
+
+// ---
+func testFuncPanic() {
+	defer func() { // defer отложить выполнение функции
+		fmt.Println("test2")
+	}()
+	func() {
+		fmt.Println("test1")
+	}()
+	panic("PANIC") // останавливает выполнение
+	fmt.Println("0")
+}
+
+// ---
+func testPointers() {
+	a := 5
+	pointerOne(&a)
+	fmt.Println(a)
+
+	b := new(int)
+	pointerTwo(b)
+	fmt.Println(*b)
+
+	c := Circle{x: 0, y: 0, r: 5}
+	fmt.Println(c.area())
+}
+func pointerOne(a *int) {
+	*a = 1
+}
+func pointerTwo(b *int) {
+	*b = 2
+}
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+// ---
+func testStruct() {
+	c1 := new(Circle)              // создаст экземпляр, присвоит значения по умолчанию и вернёт указатель
+	c2 := Circle{x: 0, y: 0, r: 5} // создаст экземпляр типа, присвоит значения
+	c3 := Circle{0, 0, 5}          // создаст экземпляр типа, присвоит значения
+	fmt.Println(*c1)
+	fmt.Println(c2.r)
+	fmt.Println(c3)
+
+	p := new(Android) // создаём экземпляр типа с имплементацией его методов
+	p.person.Talk()
+}
+
+type Person struct {
+	name string
+}
+type Android struct {
+	person Person
+	model  string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.name)
+}
+
+// ---
+func testStreams() {
+	for i := 0; i < 10; i++ {
+		go f(i) // горутина
+	}
+
+	var input string
+	fmt.Scanln(&input) // хак для отладки чтобы видеть лог
+}
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+		fmt.Println(n, ":", i, "", rand.Intn(250))
+	}
+}
+
+// ---
+// func testPackages() {
+// t := time.Now()
+// r := rand.Intn(10)
+// r := rand.New(123)
+// amt := time.Duration(rand.Intn(250))
+// fmt.Println(r)
+// }
---
+++ readme.md
@@ -0,0 +1,192 @@
+# Команды  
+
+**Запуск скрипта go**  
+`go run main.go`
+
+# Типы  
+
+**Преоброзование типов** 
+```
+float64(valueInt) // тип(value)
+```
+
+**«signed integer» (знаковое целое). Только положительное значение и 0**
+```
+int8  // -128 through 127
+int16 // -32768 through 32767
+int32 // -2147483648 through 2147483647 // псевдоним rune  
+int64 // -9223372036854775808 through 9223372036854775807
+```
+
+**«unsigned integer» (беззнаковое целое)**  
+```
+uint8  // 0 through 255 // псевдоним byte 
+uint16 // 0 through 65535
+uint32 // 0 through 4294967295
+uint64 // 0 through 18446744073709551615
+```
+
+**Машиннозависимые типы**  
+`uint, int и uintptr`  
+
+**Числа с плавающей точкой**  
+```
+float32
+float64
+```
+
+**Комплексные числа**  
+```
+complex64
+complex128
+```
+
+**Прочие типы**  
+```
+string
+boolean
+NaN
++∞
+−∞
+```
+# Выжимка
+
+## Array  
+len(array) // Получить длину массива  
+newArray := [5]float64{ 98, 93, 77, 82, 83 } // Создание массива, сокращённая запись, тип не обязателен  
+newArray := make([]float64, 5, 10) // Срез массива по индексу, где 10 длина массива на который указывает срез  
+newArray := array[0:5] // Срез массива по индексу, сокращённая запись  
+newArray := append(array, newValue) // Добавление в массив  
+copy(newArray, copyArray) // Копирование массива  
+
+## Map  
+newMap = make(map[string]int) // Мапа key:string value:int  
+newMap := map[string]string { "key1": "value1", "key2": "value2" } // сокращённая запись мапы  
+myMap["myKey"] // Получить значение мапы по ключу  
+delete(x, "myKey") // Удалить значение по ключу  
+value, ok := x["key"] // Получить значение мапы по ключу и статус наличия этого значения  
+
+## Потоки, условия  
+
+```go
+for i := 1; i <= 10; i++ {
+	// ...
+} 
+```
+```go
+if isOk {
+	// ...
+} else if {
+	// ...
+} else {
+	// ...
+} 
+```
+```go
+switch value {
+	case itValue: // ...
+	default: // ...
+}
+```
+
+## Функции  
+
+defer myFunc() // отложить выполнение функции, типо setTimeout 0 в js  
+func(){ /* ... */}() // анонимная функция  
+
+
+```go
+func myFunc(value []float64) float64 {
+	// ...
+	return result
+}
+```
+```go
+func myFunc() (int, int) {
+    return value, ok // использовать можно так x, y := myFunc()
+}
+```
+```go
+func myFunc() (value, err := f())  {
+    return value, err // возвращает значение и ошибку
+}
+```
+```go
+func myFunc(args ...int) int {
+	// ...
+	return result
+}
+```
+
+## Указатели (на ячейки памяти)  
+```go
+func main() {
+	value := 5
+	myFunc(&value) // & передаёт адрес переменной value
+	fmt.Println(value)
+
+	value := new(int) // создаём указатель
+	myFunc(value) //передаём указатель
+	fmt.Println(*value)
+
+	c := Circle{x: 0, y: 0, r: 5}
+	fmt.Println(c.area()) // вызываем, как будто у объкта есть метод area
+}
+func myFunc(value *int) { // * говорит что мы работаем с ячейкой памяти на которую ссылается value
+	*value = 0
+}
+func (c *Circle) area() float64 { // функция особого типа
+	return math.Pi * c.r * c.r
+}
+```
+```go
+type Person struct {
+	name string
+}
+type Android struct {
+	person Person
+	model string
+}
+
+func main() {
+	p := new(Android) // создаём экземпляр типа с имплементацией его методов
+    p.person.Talk()
+}
+
+func (p *Person) Talk() { // функция особого типа
+	fmt.Println("Hi, my name is", p.name)
+}
+```
+
+## Типизация, интерфесы/структуры  
+c := new(Circle) // создаст экземпляр, присвоит значения по умолчанию и вернёт указатель
+```go
+type Circle struct {
+	x float64
+	y float64
+	r float64
+}
+```
+**Вложенные структуры**  
+```go
+type Person struct {
+	name string
+}
+type Android struct {
+	person Person
+	model string
+	abilities []Abilities // где Abilities это interface с описанием методов
+}
+```
+**Интерфейсы (в них описываются методы)**  
+```go
+type Shape interface {
+    area() float64
+}
+```
+
+## Пакеты
+```go
+time.Now() // возвращает текущее время /* 2021-09-01 01:47:17.689248556 +0400 +04 m=+0.000055961 */  
+rand.Intn(10) // возвращает случайное число для последовательных итераций  
+```
